4c57ccbea8a81c11be1f8b51f82b2620eb3b0b34
LorisPolenz-UI <59690885+LorisPolenz-UI@users.noreply.github.com>
2021-03-18 18:26:44 +0100

    new Code

---
+++ linktree/src/components/button.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "button", 
+    data() {
+        return {
+            name: "", 
+            url: ""
+        }
+    }
+    
+}
+</script>
---
+++ linktree/src/components/link-node.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        Heöllo {{data}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "link-node", 
+        data() {
+            return {
+                links: [
+                    {
+                        name: "GitHub",
+                        url: "https://github.com/LorisPolenz-UI"
+                    },
+                    {
+                        name: "Twitter",
+                        url: "https://twitter.com/LorisPolenz"
+                    },
+                    {
+                        name: "LinkedIn",
+                        url: "https://www.linkedin.com/in/loris-polenz/"
+                    },
+                    {
+                        name: "Traily",
+                        url: "https://www.traily.ch"
+                    },
+                    {
+                        name: "Contact Me",
+                        url: "https://contact.lorispolenz.com"
+                    },
+                    
+                ]
+            }
+        }, 
+        computed() {
+            
+        }
+    }
+</script>
